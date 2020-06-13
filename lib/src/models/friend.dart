a7d32a119caf9f4f6a4c3e8715b275d3b54788df
Raveesh Agarwal <hi@raveeshagarwal.in>
2020-06-13 21:10:58 +0530

    update: adds friend and message models update: adds **/*.g.dart to gitignore

--- lib/src/models/friend.dart
+++ lib/src/models/friend.dart
@@ -1,1 +1,12 @@
-class Friend {}
+import 'package:json_annotation/json_annotation.dart';
+part 'friend.g.dart';
+
+@JsonSerializable()
+class Friend {
+  Friend(this.id, this.name, this.avatarUrl, this.lastMessage);
+  String id, name, avatarUrl, lastMessage;
+
+  factory Friend.fromJson(Map<String, dynamic> json) => _$FriendFromJson(json);
+
+  Map<String, dynamic> toJson() => _$FriendToJson(this);
+}
