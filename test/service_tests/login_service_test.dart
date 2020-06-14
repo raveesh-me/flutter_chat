567e0b2d389e3276f86bb35d29deab4aee7feb99
Raveesh Agarwal <hi@raveeshagarwal.in>
2020-06-14 20:32:28 +0530

    update: start by adding service files

---
+++ test/service_tests/login_service_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  group("Services related to login should work properly", () {
+    test("Existing uer should be able to login", () {});
+  });
+}
