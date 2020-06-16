05d8ebc7de3e3be0ca66a68ccee25f7b96c13b24
Raveesh Agarwal <hi@raveeshagarwal.in>
2020-06-16 15:00:09 +0530

    update: OpEnvironment enum, implement create_service

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:simpleholmuskchat/src/app.dart';
 
+enum OpEnvironments{demo, test, production}
+const OpEnvironments opEnvironment = OpEnvironments.demo;
+
 main() {
   runApp(MyApp());
 }
--- lib/src/service/api/create_service.dart
+++ lib/src/service/api/create_service.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
-
 import 'package:flutter/services.dart';
 import "package:http/http.dart" as http;
+import 'package:simpleholmuskchat/src/service/api/http_error_handler.dart';
 import 'package:simpleholmuskchat/src/service/api/url_options.dart';
 
 /// Unauthenticated, does not need a token
@@ -9,21 +9,21 @@ class CreateService {
   UrlOptions urlOptions;
   final String path = '/create';
   init() async {
-    urlOptions = await UrlOptions.fromJson(
-        json.decode(await rootBundle.loadString('env/env.json')));
+    urlOptions = UrlOptions.fromJson(
+      json.decode(await rootBundle.loadString('env/env.json'))["production"],
+    );
   }
 
   Future<String> createAccountAndGetToken(String email, String password) async {
     if (urlOptions == null) await init();
-    final http.Response response = await http.get(urlOptions.baseUrl + path);
-    switch (response.statusCode) {
-      case 200:
-        return json.decode(response.body)["token"];
-        break;
-      case 400:
-      case 401:
-      case 404:
-      case 400:
-    }
+    final http.Response response =
+        await http.post(urlOptions.baseUrl + path, body: {
+      email: email,
+      password: password,
+    });
+    if (response.statusCode == 200)
+      return json.decode(response.body)["token"];
+    else
+      return HttpErrorHandler.handleResponseError(response);
   }
 }
